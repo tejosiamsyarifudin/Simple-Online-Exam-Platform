94fb69c2f16501afd727c195167aed5d53ea34c6
tejosiamsyarifudin <145649685+tejosiamsyarifudin@users.noreply.github.com>
2023-11-28 12:31:28 +0700

    Validate credentials & redirect to dashboard

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,5 @@
 import { LoginForm } from '../../components/LoginForm/LoginForm';
 
-// Login Page:
-// a. Create a secure login page featuring a form for entering a username and
-// password. (done)
-// b. Implement authentication logic to validate the entered credentials, utilizing
-// placeholder data for testing purposes. (progress)
-// c. Upon successful login, redirect the student to the dashboard page.
-
 export default function LoginPage() {
   return (
     <LoginForm />
--- components/LoginForm/LoginForm.tsx
+++ components/LoginForm/LoginForm.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { loginUser } from '@/app/api/auth/login';
 import classes from './LoginForm.module.css';
 import {
   TextInput,
@@ -9,8 +12,50 @@ import {
   Container,
   Title,
 } from '@mantine/core';
+import { useForm } from '@mantine/form';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation'
 
 export function LoginForm() {
+  const router = useRouter()
+  const form = useForm({
+    initialValues: { username: 'eve.holt@reqres.in', password: 'cityslicka' },
+    validate: {
+      username: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid username'),
+      password: (val) => (val.length <= 6 ? 'Password should include at least 6 characters' : null),
+    },
+  });
+
+  const handleLogin = async () => {
+    try {
+      const response = await loginUser({
+        email: form.values.username,
+        password: form.values.password,
+      });
+      const token = response.token;
+      localStorage.setItem('token', token);
+      form.reset();
+      console.log('Login successful');
+      isLogin()
+
+    } catch (error) {
+      console.error(error);
+      alert('Invalid username and password');
+    }
+  };
+
+  const isLogin = async () => {
+    const storedToken = localStorage.getItem('token');
+    console.log(storedToken)
+    if (storedToken) {
+      router.push('/dashboard');
+    }
+  };
+
+  useEffect(() => {
+    isLogin()
+  }, []);
+
 
   return (
     <Container size={420} my={40}>
@@ -18,22 +63,28 @@ export function LoginForm() {
         Portal Sekolah
       </Title>
       <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-        <form>
+        <form onSubmit={form.onSubmit(handleLogin)}>
           <Stack>
             <TextInput
               required
               label="Username"
-              placeholder="tejosiam@gmail.com"
+              placeholder="eve.holt@reqres.in"
               radius="md"
               data-testid="username"
+              value={form.values.username}
+              onChange={(event) => form.setFieldValue('username', event.currentTarget.value)}
+              error={form.errors.username && 'Invalid username'}
             />
             <PasswordInput
               required
               label="Password"
-              placeholder="Visi123##"
+              placeholder="cityslicka"
               radius="md"
               className="password"
               data-testid="password"
+              value={form.values.password}
+              onChange={(event) => form.setFieldValue('password', event.currentTarget.value)}
+              error={form.errors.password && 'Password should include at least 6 characters'}
             />
           </Stack>
           <Group justify="space-between" mt="xs">
@@ -44,6 +95,5 @@ export function LoginForm() {
         </form>
       </Paper>
     </Container>
-
   );
 }
